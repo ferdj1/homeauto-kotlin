af95f031923ac7fe41240788400445c1b6b537f4
ferdj1 <ferdj1@outlook.com>
2018-10-06 17:56:24 +0200

    Init commit

---
+++ src/main/kotlin/ferdj1/projects/homeauto/HomeautoApplication.kt
@@ -0,0 +1,11 @@
+package ferdj1.projects.homeauto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeautoApplication
+
+fun main(args: Array<String>) {
+    runApplication<HomeautoApplication>(*args)
+}
---
+++ src/test/kotlin/ferdj1/projects/homeauto/HomeautoApplicationTests.kt
@@ -0,0 +1,16 @@
+package ferdj1.projects.homeauto
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HomeautoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
